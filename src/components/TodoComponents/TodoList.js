be04ee36eb686df3c52e1974320e7991476909b3
Reyaad Rafeek <reyaad510@gmail.com>
2019-03-05 17:22:26 -0500

    Created state, data array, and import ToDoList.js boiler plate

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,15 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+
+import React from 'react';
+
+function ToDoList() {
+    return (
+        <div>
+            <h1>ToDo List: MVP</h1>
+        </div>
+    );
+}
+
+export default ToDoList;
+
